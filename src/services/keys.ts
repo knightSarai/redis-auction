f747428edb2b3ed0a6e222c29adc07973c002c9e
knightSarai <knight.sarai.dev@gmail.com>
2023-10-14 19:11:52 +0300

    Add queries to create items

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,2 +1,4 @@
 export const pageCacheKey = (id: string) => 'pagecache#' + id;
+export const userKey = (id: string) => 'user#' + id;
+export const sessionKey = (id: string) => 'session#' + id;
 export const itemKey = (id: string) => 'item#' + id;
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,48 @@
+import { client } from '$services/redis';
+import { genId } from  '$services/utils';
+import { itemKey } from '$services/keys';
 import type { CreateItemAttrs } from '$services/types';
+import { deserialize } from '$services/queries/items/deserialize';
 
-export const getItem = async (id: string) => {};
 
-export const getItems = async (ids: string[]) => {};
+export const getItem = async (id: string) => {
+    const item = await client.hGetAll(itemKey(id));
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+    if (!Object.keys(item).length) {
+        return null;
+    }
+
+    return deserialize(id, item);
+
+};
+
+
+export const getItems = async (ids: string[]) => {
+    const commands = ids.map(id => client.hGetAll(itemKey(id)));
+
+    const items = await Promise.all(commands);
+
+    return items.map((item, idx) => {
+        if (!Object.keys(item).length) {
+            return null;
+        }
+
+        return deserialize(ids[idx], item);
+    });
+
+
+};
+
+
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+    const id = genId();
+
+    await client.hSet(itemKey(id), {
+        ...attrs,
+        createdAt: attrs.createdAt.toMillis(),
+        endingAt: attrs.endingAt.toMillis(),
+    });
+
+    return id;
+
+};

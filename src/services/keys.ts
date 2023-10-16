5caaf79f453c0de703a8c83dd1e4b39d744fc834
knightSarai <knight.sarai.dev@gmail.com>
2023-10-16 21:26:17 +0300

    Implement like system

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,5 +1,7 @@
 export const pageCacheKey = (id: string) => 'pagecache#' + id;
 export const userKey = (id: string) => 'user#' + id;
 export const sessionKey = (id: string) => 'session#' + id;
-export const itemKey = (id: string) => 'item#' + id;
 export const usernamesListKey = () => 'usernames:list'
+export const itemKey = (id: string) => 'item#' + id;
+export const usersLikesKey = (userId: string) => 'users:likes#' + userId;
+
--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,38 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { usersLikesKey, itemKey } from '$services/keys';
+import { getItems } from './items';
 
-export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return await client.sIsMember(usersLikesKey(userId), itemId);
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+};
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const likedItems = async (userId: string) => {
+    const itemIds = await client.sMembers(usersLikesKey(userId));
+
+    return getItems(itemIds);
+};
+
+export const likeItem = async (itemId: string, userId: string) => {
+    const added = await client.sAdd(usersLikesKey(userId), itemId);
+
+    if (added) {
+        await client.hIncrBy(itemKey(itemId), 'likes', 1);
+    }
+
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(usersLikesKey(userId), itemId);
+
+    if (removed) {
+        await client.hIncrBy(itemKey(itemId), 'likes', -1);
+    }
+
+};
+
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const ids = await client.sInter([usersLikesKey(userOneId), usersLikesKey(userTwoId)]);
+    return getItems(ids);
+};

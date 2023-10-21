d1b61fd77d006c0ca3e58e494c45b21e84113746
knightSarai <knight.sarai.dev@gmail.com>
2023-10-21 17:45:51 +0300

    Add signin

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,7 +1,8 @@
 export const pageCacheKey = (id: string) => 'pagecache#' + id;
 export const userKey = (id: string) => 'user#' + id;
 export const sessionKey = (id: string) => 'session#' + id;
-export const usernamesListKey = () => 'usernames:list'
+export const usernamesUniqueKey = () => 'usernames:unique'
+export const usernamesKey = () => 'usernames'
 export const itemKey = (id: string) => 'item#' + id;
 export const usersLikesKey = (userId: string) => 'users:likes#' + userId;
 
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,15 +1,25 @@
 import { client } from '$services/redis';
-import { userKey, usernamesListKey } from '$services/keys';
+import { userKey, usernamesKey, usernamesUniqueKey } from '$services/keys';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+    let userId: number | string = await client.zScore(usernamesKey(), username);
+
+    if (!userId) {
+        throw new Error('User not found');
+    };
+
+    return getUserById((userId).toString(16));
+
+};
 
 export const getUserById = async (id: string) => {
     const user = await client.hGetAll(userKey(id));
 
+
     return {
-        id: user.id,
+        id,
         ...user,
     };
 };
@@ -17,7 +27,7 @@ export const getUserById = async (id: string) => {
 export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId();
 
-    const exist = await client.sIsMember(usernamesListKey(), attrs.username);
+    const exist = await client.sIsMember(usernamesUniqueKey(), attrs.username);
     
     if (exist) {
         throw new Error('Username already exists');
@@ -28,7 +38,11 @@ export const createUser = async (attrs: CreateUserAttrs) => {
         password: attrs.password,
     })
 
-    await client.sAdd(usernamesListKey(), attrs.username);
+    await client.sAdd(usernamesUniqueKey(), attrs.username);
+    await client.zAdd(usernamesKey(), {
+        value: attrs.username,
+        score: parseInt(id, 16)
+    })
 
     return id;
 };

16cc4b578e75a381b6f4244471cd68e86d477994
knightSarai <knight.sarai.dev@gmail.com>
2023-10-22 12:17:08 +0300

    Bids history

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -9,3 +9,4 @@ export const itemsKey = (id: string) => 'items#' + id;
 export const itemsByViewsKey = () => 'items:views';
 export const itemsByEndingAtKey = () => 'items:endingAt';
 export const itemsViewsKeys = (id: string) => 'items:views#' + id;
+export const bidHistoryKey = (id: string) => 'bidHistory#' + id;
--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,7 +1,34 @@
+import { client } from '$services/redis';
+import { bidHistoryKey } from '$services/keys';
+import { DateTime } from 'luxon';
 import type { CreateBidAttrs, Bid } from '$services/types';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+    return await client.rPush(
+        bidHistoryKey(attrs.itemId),
+        serializeHistory(
+            attrs.amount,
+            attrs.createdAt.toMillis()
+        )
+    );
+
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+    const startIdx = -1 * offset - count;
+    const endIdx = -1 - offset;
+    const history = await client.lRange(bidHistoryKey(itemId), startIdx, endIdx);
+    return history.map(deserializeHistory);
 };
+
+
+const serializeHistory = (amount: number, createdAt: number) => amount + ':' + createdAt;
+
+const deserializeHistory = (storedValue: string) => {
+    const [amount, createdAt] = storedValue.split(':');
+    return {
+        amount: parseFloat(amount),
+        createdAt: DateTime.fromMillis(parseInt(createdAt)),
+    };
+
+}
